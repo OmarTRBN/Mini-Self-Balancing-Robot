77ba96db06c78df8398a1de536bf63c8ec89c195
OmarTRBN <omar.tarabin2004@gmail.com>
2024-06-19 14:24:19 +0300

    Designed case for Li-Po batteries.

---
+++ SBR/main/KalmanFilter.h
@@ -0,0 +1,26 @@
+#ifndef KalmanFilter_h
+#define KalmanFilter_h
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+class KalmanFilter
+{
+    public:
+
+	KalmanFilter(double angle = 0.001, double bias = 0.003, double measure = 0.03);
+	double update(double newValue, double newRate);
+
+    private:
+
+	double Q_angle, Q_bias, R_measure;
+	double K_angle, K_bias, K_rate;
+	double P[2][2], K[2];
+	double S, y;
+	double dt, kt;
+};
+
+#endif
--- SBR/main/main.ino
+++ SBR/main/main.ino
@@ -1,75 +1,62 @@
+#include <Wire.h>
 #include "mpu.h"
+#include "KalmanFilter.h"
 
-float roll = 0.0, pitch = 0.0, yaw = 0.0, rollAcc;
-float accX, accY, accZ, gyroX, gyroY, gyroZ;
+MPU6050 mpu;
+
+KalmanFilter kalmanX(0.001, 0.003, 0.03);
+KalmanFilter kalmanY(0.001, 0.003, 0.03);
+
+float accPitch = 0;
+float accRoll = 0;
+
+float kalPitch = 0;
+float kalRoll = 0;
 
 void setup() 
 {
   Serial.begin(9600);
-    initMpu();
+
+  // Initialize MPU6050
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    delay(500);
+  }
+  mpu.calibrateGyro();
 }
 
 void loop()
 {
-  
-    getMpuValues(&accX, &accY, &accZ, &gyroX, &gyroY, &gyroZ);
-//    rollAcc=atan(accY/sqrt(accX*accX+accZ*accZ))*1/(3.142/180.0);
-//    kalmanFilter(&roll, rollAcc, gyroX);
-    
-    printData();
-    delay(1);
-}
+  Vector acc = mpu.readNormalizeAccel();
+  Vector gyr = mpu.readNormalizeGyro();
 
-void kalmanFilter(float *angleKalman, float Zk, float newRate)
-{   
-    static float Q_angle = 0.003, bias = 0.0, Q_bias = 0.00005, R_measure=0.003;
-    static float S, K[2], P[2][2]={{0, 0}, {0, 0}}, P00_temp, P01_temp, y, dt;
-    static uint32_t currentKalmanTime, lastKalmanTime=0.0;
-    
-    currentKalmanTime = millis();
-    dt = (currentKalmanTime - lastKalmanTime) / 1000.0;
-    lastKalmanTime = currentKalmanTime; 
-    
-    // Predict X(k+1,k) = F*X(k,k)
-    *angleKalman += dt * (newRate - bias);
+  // Calculate Pitch & Roll from accelerometer (deg)
+  accPitch = -(atan2(acc.XAxis, sqrt(acc.YAxis*acc.YAxis + acc.ZAxis*acc.ZAxis))*180.0)/M_PI;
+  accRoll  = (atan2(acc.YAxis, acc.ZAxis)*180.0)/M_PI;
 
-    // Update error covariance matrix P(k+1,k) = F*P(k,k)*F_T + Q
-    P[0][0] += dt * (dt*P[1][1] - P[0][1] - P[1][0] + Q_angle);
-    P[0][1] -= dt * P[1][1];
-    P[1][0] -= dt * P[1][1];
-    P[1][1] += Q_bias * dt;
-    
-    // Compute Kalman gain
-    S = 1.0 / (P[0][0] + R_measure);
-    K[0] = P[0][0] * S;
-    K[1] = P[1][0] * S;
+  // Kalman filter
+  kalPitch = kalmanY.update(accPitch, gyr.YAxis);
+  kalRoll = kalmanX.update(accRoll, gyr.XAxis);
 
-    // Update estimate with measurement zk (newAngle)
-    y = Zk - *angleKalman;
-    *angleKalman += K[0] * y;
-    bias += K[1] * y;
-    
-    P[0][0] -= K[0] * P00_temp;
-    P[0][1] -= K[0] * P01_temp;
-    P[1][0] -= K[1] * P00_temp;
-    P[1][1] -= K[1] * P01_temp;
-}
+//  Serial.print(accPitch);
+//  Serial.print(":");
+//  Serial.print(accRoll);
+//  Serial.print(":");
+  Serial.print(kalPitch);
+//  Serial.print(":");
+//  Serial.print(kalRoll);
+//  Serial.print(":");
+//  Serial.print(acc.XAxis);
+//  Serial.print(":");
+//  Serial.print(acc.YAxis);
+//  Serial.print(":");
+//  Serial.print(acc.ZAxis);
+//  Serial.print(":");
+//  Serial.print(gyr.XAxis);
+//  Serial.print(":");
+//  Serial.print(gyr.YAxis);
+//  Serial.print(":");
+//  Serial.print(gyr.ZAxis);
 
-void printData()
-{
-//    Serial.print("Roll: ");
-//    Serial.println(roll);
-//    Serial.print( "Pitch: ");
-//    Serial.print(pitch);
-//    Serial.print(" Yaw: ");
-//    Serial.println(yaw);
-     Serial.print("aX = "); Serial.print(accX);
-    Serial.print(" | aY = "); Serial.print(accY);
-    Serial.print(" | aZ = "); Serial.print(accZ);
-    Serial.print(" | gX = "); Serial.print(gyroX);
-    Serial.print(" | gY = "); Serial.print(gyroY);
-    Serial.print(" | gZ = "); Serial.print(gyroZ);
-
-     Serial.print(" | Time = ");
-     Serial.println(micros());
+  Serial.println();
 }
